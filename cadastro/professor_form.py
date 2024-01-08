cccf8e4a900880cc39279266e6d97d18b71d74c5
marqueane <marqueane_64@hotmail.com>
2024-01-08 20:27:02 -0300

    Configurado formulário 'modelform'

---
+++ cadastro/professor_form.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from cadastro.models import Professor
+
+class FormProfessor(ModelForm):
+    class Meta:
+        model = Professor
+        fields = '__all__'
---
+++ cadastro/tests/test_home.py
@@ -0,0 +1,3 @@
+def test_meu_primeiro_test():
+    assert True
+
