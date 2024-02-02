cce9eb17cdb17c8bcd14acaa33ae00a93bba0ae3
marqueane <marqueane_64@hotmail.com>
2024-02-02 20:22:50 -0300

    Atualização do formulário HTML utilizando ModelForm

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Professor(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,44 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from cadastro.professor_form import FormProfessor
 from .models import Professor, Funcao
 
 
 def add_professor(request):
-    if request.method == 'GET':
-    #Se for uma requisição GET, renderiza o formulário de cadastro
-        return render(request, 'cadastro/cadastro.html')
-    elif request.method == 'POST':
-    #Se for uma requisição POST, obtém os dados do formulário
-        nome = request.POST.get('nome')
-        sobrenome = request.POST.get('sobrenome')
-        cargo = request.POST.get('cargo')
-        matricula = request.POST.get('matricula')
-        funcao = request.POST.getlist('funcao')
-        lotacao = request.POST.getlist('lotação')
-        turno = request.POST.getlist('turno')
+    if request.method == "GET":
+        # Se for uma requisição GET, renderiza o formulário de cadastro
+        return render(request, "cadastro/cadastro.html", {'form':FormProfessor()})
+    elif request.method == "POST":
+        # Se for uma requisição POST, obtém os dados do formulário
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        cargo = request.POST.get("cargo")
+        matricula = request.POST.get("matricula")
+        funcoes = request.POST.getlist("funcao")
+        lotacoes = request.POST.getlist("lotação")
+        turnos = request.POST.getlist("turno")
 
-        #Validação Matrícula
+        # Validação Matrícula
         professor = Professor.objects.filter(matricula=matricula)
 
         if professor.exists():
-            return render(request,'cadastro/cadastro.html', {'nome':nome, 'sobrenome': sobrenome, 'cargo': cargo, 'funcao': funcao})
-        
-   
-        #Adiciona um novo professor no banco de dados
+            return render(
+                request,
+                "cadastro/cadastro.html",
+                {
+                    "nome": nome,
+                    "sobrenome": sobrenome,
+                    "cargo": cargo,
+                    "funcoes": (funcoes, lotacoes, turnos),
+                    'form':FormProfessor()
+                },
+               
+            )
+
+        # Adiciona um novo professor no banco de dados
         professor = Professor(
-            nome = nome,
-            sobrenome = sobrenome,
-            cargo = cargo,
-            matricula = matricula
+            nome=nome, sobrenome=sobrenome, cargo=cargo, matricula=matricula
         )
-        #Salva o professor no banco de dados
+        # Salva o professor no banco de dados
         professor.save()
 
-        #Cria e salva as funções associadas ao professor
-        #zip: cria uma tupla, dentro de uma lista que co-relaciona informações
-        for funcao, lotacao, turno in zip(funcao, lotacao, turno):
-            func = Funcao(funcao=funcao, lotacao=lotacao, turno=turno, professor=professor)
+        # zip: cria uma tupla, dentro de uma lista que co-relaciona informações
+        for funcao, lotacao, turno in zip(funcoes, lotacoes, turnos):
+            func = Funcao(
+                funcao=funcao, lotacao=lotacao, turno=turno, professor=professor
+            )
             func.save()
 
-        #Retorna uma resposta HTTP simples indicando sucesso (pode ser personalizado)
-        return HttpResponse('Cadastro realizado com sucesso')
+        # Retorna uma resposta HTTP simples indicando sucesso (pode ser personalizado)
+        return HttpResponse("Cadastro realizado com sucesso")
