4944d2124b6a91d783827f7c03143ffe8fcc15f6
marqueane <marqueane_64@hotmail.com>
2024-02-02 21:04:59 -0300

    Adiciona opções no campo 'cargo' do formulário

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
+CHOICES_CARGO = [
+    ('', 'Selecione um cargo'),
+    ('peb', 'Professor de Séries Inicias'),
+    ('aee', 'Professor de Atendimento Educacional Especializado'),
+    ('apoio', 'Professor de Apoio Pedagógico'),
+    ('português', 'PEB - Língua Portuguesa'),
+    ('matemática', 'PEB - Matemática'),
+    ('geografia', 'PEB - Geografia'),
+    ('ciências', 'PEB - Ciências'),
+    ('ed_fisica', 'PEB - Ed. Física'),
+    ('arte', 'PEB - Arte'),
+    ('ed_religiosa', 'PEB - Educação Religiosa'),
+    ('ingles', 'PEB - Língua Inglesa'),
+    ('musica', 'Professor de Música'),
+]
+
+
+
 class Professor(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     matricula = models.CharField(max_length=8, unique=True)
-    cargo = models.CharField(max_length=50)
+    cargo = models.CharField(choices=CHOICES_CARGO,default="", max_length=50)
 
     def __str__(self) -> str:
         return self.nome
--- cadastro/professor_form.py
+++ cadastro/professor_form.py
@@ -6,3 +6,11 @@ class FormProfessor(ModelForm):
     class Meta:
         model = Professor
         fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'sobrenome': forms.TextInput(attrs={'class': 'form-control'}),
+            'cargo': forms.Select(attrs={'class': 'form-control form-select'}),
+            'matricula': forms.TextInput(attrs={'class': 'form-control'}),
+
+
+        }
